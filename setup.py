1cd0e67482863f6885f7b125b92ace1018a15599
Joan Gonzalez <jgquiroga@dc.uba.ar>
2024-12-12 17:46:47 -0300

    Actualizo dependencias

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ setup(
     name = 'googledrive',
     version = '0.1',
     packages = find_packages(),
-    install_requires=['slepy>=0.1', 'polars>=1.17.1'],
+    install_requires=['slepy>=0.1', 'polars>=1.17.1', "google-api-python-client>=2.155.0", "google-auth-httplib2>=0.2.0", "google-auth-oauthlib>=1.2.1"],
 )
