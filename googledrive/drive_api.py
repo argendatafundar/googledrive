ac9fb3b37a393f4a22313071eec9c2129c8221ee
Joan Gonzalez <jgquiroga@dc.uba.ar>
2025-02-05 11:17:00 -0300

    .token no es classproperty

--- googledrive/drive_api.py
+++ googledrive/drive_api.py
@@ -174,12 +174,6 @@ def execute_service_method(f: Callable):
         
         return _
 
-class classproperty(property):
-    """Utilidad para crear propiedades de clase"""
-
-    def __get__(self, cls, owner):
-        return classmethod(self.fget).__get__(None, owner)()
-
 @inject_logger
 class DriveFilesApi(Singleton):
     log: Logger
@@ -202,7 +196,7 @@ class DriveFilesApi(Singleton):
         )
         self.log.info("Authenticated to Google Drive.")
 
-    @classproperty
+    @property
     def token(self):
         return self._drive._http.credentials.token
     
